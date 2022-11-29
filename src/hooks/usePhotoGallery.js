4ceec3bcc4e805b151acda2d77711d3517a532dc
Karthikeyan S <skarthikeyan2901@gmail.com>
2022-11-29 22:05:52 +0530

    blob image obtained

--- src/hooks/usePhotoGallery.js
+++ src/hooks/usePhotoGallery.js
@@ -1,23 +1,27 @@
-// import { useState, useEffect } from "react";
-// import { isPlatform } from "@ionic/react";
-
 import {
   Camera,
   CameraResultType,
   CameraSource,
   Photo,
 } from "@capacitor/camera";
-// import { Filesystem, Directory } from "@capacitor/filesystem";
-// import { Preferences } from "@capacitor/preferences";
-// import { Capacitor } from "@capacitor/core";
 
 export function usePhotoGallery() {
+
+    const savePicture = async (photo) => {
+        const savedFile = await fetch(photo.webPath);
+        const savedFileBlob = await savedFile.blob();
+        return savedFileBlob;
+    };
+
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100,
     });
+
+    const fileName = new Date().getTime() + ".png";
+    const savedFileImage = await savePicture(photo);
   };
 
   return {
